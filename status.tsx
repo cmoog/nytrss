1796ded4ba400db09795f5e895b6c16d012b2f42
Charlie Moog <moogcharlie@gmail.com>
2022-11-21 08:48:12 -0600

    switch to twind for status page

--- status.tsx
+++ status.tsx
@@ -1,15 +1,24 @@
 import render from "preact-render-to-string";
 import { FunctionComponent } from "preact";
+import { setup, tw } from "twind";
+import { virtualSheet, getStyleTag } from "twind/sheets";
+
+const sheet = virtualSheet();
+setup({ sheet });
 
 export function renderStatusPage(errors: Map<string, any>): string {
+  sheet.reset();
+  const body = render(<Page status={errors} />);
+  const styleTag = getStyleTag(sheet);
+
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://cdn.tailwindcss.com"></script>
+${styleTag}
 </head>
-<body>${render(<Page status={errors} />)}</body>
+<body>${body}</body>
 </html>
 `;
 }
@@ -23,7 +32,7 @@ const Page: FunctionComponent<{ status: Map<string, unknown> }> = ({
 );
 
 const Layout: FunctionComponent = ({ children }) => (
-  <main className="pt-4 px-4 max-w-4xl mx-auto flex flex-col items-center">
+  <main className={tw`pt-4 px-4 max-w-4xl mx-auto flex flex-col items-center`}>
     {children}
   </main>
 );
@@ -31,26 +40,28 @@ const Layout: FunctionComponent = ({ children }) => (
 const Table: FunctionComponent<{ status: Map<string, unknown> }> = ({
   status,
 }) => (
-  <table className="min-w-full table-auto border-collapse border-spacing-2 border">
-    <thead className="text-left">
-      <tr className="border">
-        <th className="border p-2">feed</th>
-        <th className="border p-2">mirror status</th>
+  <table
+    className={tw`min-w-full table-auto border-collapse border-spacing-2 border`}
+  >
+    <thead className={tw`text-left`}>
+      <tr className={tw`border`}>
+        <th className={tw`border p-2`}>feed</th>
+        <th className={tw`border p-2`}>mirror status</th>
       </tr>
     </thead>
     <tbody>
       {Array.from(status.keys()).map((key) => (
-        <tr key={key} className="border">
-          <td className="p-2 border hover:underline">
+        <tr key={key} className={tw`border`}>
+          <td className={tw`p-2 border hover:underline`}>
             <a
-              className="text-blue-600 visited:text-purple-600"
+              className={tw`text-blue-600 visited:text-purple-600`}
               href={`/${key}`}
               target="_blank"
             >
               /{key}
             </a>
           </td>
-          <td className="p-2 border">{status.get(key) || "success"}</td>
+          <td className={tw`p-2 border`}>{status.get(key) || "success"}</td>
         </tr>
       ))}
     </tbody>
